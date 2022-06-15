1526322810669f176ff8611b72e41deac4040836
Esteban Heredia <estebanhdg@gmail.com>
2022-06-14 21:36:21 -0300

    agregando arrays

--- js/main.js
+++ js/main.js
@@ -17,20 +17,34 @@
 //dejo comentado esto para cuando este mas avanzado use todo
 
 
+// let Nturno = prompt ("A nombre de quien te gustaria reservar el turno?");
+
+// console.log ("El turno de " + Nturno + " fue agendado con exito");
+
+// let costoturn = parseInt(prompt("¿Cuanto te costo el turno?"));
+// const costoapp = 150;
+// let costofinal = 0;
+
+// function sumacosto (costoturn,costoapp){
+//     costofinal = costoturn + costoapp;
+// } 
+
+// sumacosto (costoturn, costoapp)
+
+// console.log ("El costo total del turno seria " + costofinal)
+
+// sigo comentando codigo para cuando junte todo en la segunda entrega final que mas o menos ya le voy viendo como combiar todo gracias a los arrays 
+
 let Nturno = prompt ("A nombre de quien te gustaria reservar el turno?");
+let Hturno = prompt ("En que hoario te gustaria el turno?");
 
-console.log ("El turno de " + Nturno + " fue agendado con exito");
+const InfoTurno = [Nturno + " " + Hturno];
+const TurnosDisp = [];
 
-let costoturn = parseInt(prompt("¿Cuanto te costo el turno?"));
-const costoapp = 150;
-let costofinal = 0;
+console.log ("Eturno de " + InfoTurno + " fue agendado con exito");
 
-function sumacosto (costoturn,costoapp){
-    costofinal = costoturn + costoapp;
-} 
+TurnosDisp.push(InfoTurno);
 
-sumacosto (costoturn, costoapp)
+console.log (TurnosDisp);
 
-console.log ("El costo total del turno seria " + costofinal)
-
- 
+//lo que no se me ocurre como puede ser como darle un espacio definido al array, onda de los 30 dias del calendario 24 estan disponibles y una vez que el usuario haya elegido quede cargado ya ese arrays y todos los demas libres... o sino crear 24 arrays con id para que cuando selecione une ya quede cargado ese arrays con todos los datos. nose si me explico.
