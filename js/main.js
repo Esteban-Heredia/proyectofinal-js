40c663a2f2d8498949ad00758b3048579fe45c19
Esteban Heredia <estebanhdg@gmail.com>
2022-06-16 20:48:33 -0300

    primera entrega

--- js/main.js
+++ js/main.js
@@ -1,50 +1,67 @@
-// let nombres = parseInt ( prompt ("Cuantas personas son?"));
-// let num = 0;
-
-// for (let num = 1; num <= nombres ; num = num + 1) {
-//     console.log (num)
-    
-// }
-
-// let personas = prompt ("ingresa los nombres");
-
-// while (personas != "salir") {
-//     console.log ("el nombre ingresado es " + personas);
-
-//     personas = prompt("ingresa los nombres o poner salir del programa escribir salir")
-// }
-
-//dejo comentado esto para cuando este mas avanzado use todo
-
-
-// let Nturno = prompt ("A nombre de quien te gustaria reservar el turno?");
-
-// console.log ("El turno de " + Nturno + " fue agendado con exito");
-
-// let costoturn = parseInt(prompt("¿Cuanto te costo el turno?"));
-// const costoapp = 150;
-// let costofinal = 0;
-
-// function sumacosto (costoturn,costoapp){
-//     costofinal = costoturn + costoapp;
-// } 
-
-// sumacosto (costoturn, costoapp)
-
-// console.log ("El costo total del turno seria " + costofinal)
-
-// sigo comentando codigo para cuando junte todo en la segunda entrega final que mas o menos ya le voy viendo como combiar todo gracias a los arrays 
-
 let Nturno = prompt ("A nombre de quien te gustaria reservar el turno?");
-let Hturno = prompt ("En que hoario te gustaria el turno?");
 
-const InfoTurno = [Nturno + " " + Hturno];
-const TurnosDisp = [];
+let TurnosDisp = [{dia: "lunes", id: 1 },{dia: "martes" , id: 2 },{dia: "miercoles" , id: 3 },{dia: "jueves", id: 4 },{dia: "viernes" , id: 5}];
 
-console.log ("Eturno de " + InfoTurno + " fue agendado con exito");
+let Hturno = [];
 
-TurnosDisp.push(InfoTurno);
+while (Nturno != "salir") {
+    
+    TurnosDisp = prompt ("¿De lunes a viernes que dia te gustaria el turno?")
 
-console.log (TurnosDisp);
+    Hturno = prompt ("¿En que hoario te gustaria el turno?")
 
-//lo que no se me ocurre como puede ser como darle un espacio definido al array, onda de los 30 dias del calendario 24 estan disponibles y una vez que el usuario haya elegido quede cargado ya ese arrays y todos los demas libres... o sino crear 24 arrays con id para que cuando selecione une ya quede cargado ese arrays con todos los datos. nose si me explico.
+    let costoturn = parseInt(prompt("¿Cuanto te costo el turno?"));
+    
+    // funcion para el costo del turno
+    function sumacosto (costoturn,costoapp){
+        costofinal = costoturn + costoapp;
+    }
+
+    let costofinal = 0;
+    const costoapp = 150;
+    
+    sumacosto (costoturn, costoapp)
+    
+    console.log ( "Turno para: " + Nturno);
+    console.log ( "Dia: " + TurnosDisp);
+    console.log ( "Hora: " + Hturno);
+    console.log ( "Costo total: " + costofinal)
+    
+    alert ("Guardado con exito el turno de " + Nturno + " el dia " + TurnosDisp + "a la hora " + Hturno + " Muchas gracias!")
+    
+    Nturno = prompt("Una vez registrado el turno escribir salir")
+}
+
+// const DiaTurno = [];
+
+// let Hturno = prompt ("En que hoario te gustaria el turno?");
+
+// const InfoTurno = [Nturno + " a las " + Hturno];
+
+// console.log ("El turno de " + InfoTurno + " fue agendado con exito");
+
+// TurnosDisp.push(InfoTurno);
+
+// console.log (TurnosDisp);
+
+
+
+
+// let entrada = prompt ("En que horario te gustaria el turno: 1- Mañana 2- Siesta 3- Tarde") ;
+// while (entrada != "esc") {
+//     switch (entrada) {
+//         case "1":
+//             alert("Mañana");
+//             break;
+//         case "2":
+//             alert("Siesta");
+//             break;
+//         case "3":
+//             alert("Tarde");
+//             break;
+//         default:
+//             alert ("error");
+//             break;
+//     entrada = prompt ("seleciones producto del 1 al 4")
+//     }
+// }
